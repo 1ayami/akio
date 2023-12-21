80f4396095f2ff369ea1be572d5eff09baa3a068
ayami <ayami101106@gmail.com>
2023-12-21 14:12:07 -0500

    Slash command del comando top agregado

--- src/Commands/Info/top.js
+++ src/Commands/Info/top.js
@@ -73,17 +73,64 @@ module.exports = {
 			msg.reply({ embeds: [topEmbed] })
 		},
 	},
-	// slash_command: {
-	//     name: '',
-	//     type: 1,
-	//     description: '',
-	//     options: [],
+	slash_command: {
+		name: 'top',
+		type: 1,
+		description: 'Mira el top 10 del servidor',
+		options: [],
 
-	//     /**
-	//      *
-	//      * @param { AKIO } bot
-	//      * @param { ChatInputCommandInteraction } int
-	//      */
-	//     exe(bot, int) {},
-	// },
+		/**
+		 *
+		 * @param { AKIO } bot
+		 * @param { ChatInputCommandInteraction } int
+		 */
+		async exe(bot, int) {
+			const topEmbed = new EmbedBuilder()
+				.setColor(bot.config.colors.normal)
+				.setTitle('<:purplearrow:1129823325170958457> Top 10')
+				.setThumbnail(int.guild.iconURL())
+
+			const topUsers = await levelsModel
+				.find()
+				.sort({ level: -1, xp: -1 })
+				.limit(10)
+
+			const userPosition =
+				(await (
+					await levelsModel.find().sort({ level: -1, xp: -1 })
+				).findIndex((u) => u.userID == int.user.id)) + 1
+
+			topEmbed.setFooter({ text: `Tú estas en el top #${userPosition}` })
+
+			const top3 = topUsers.slice(0, 3)
+			const top4to10 = topUsers.slice(3, 10)
+
+			const medals = [
+				'<:top_one:1187457113455333476>',
+				'<:top_two:1187457147072684033>',
+				'<:top_three:1187457182149652573>',
+			]
+
+			const fieldsTop = []
+
+			for (const [index, user] of top3.entries()) {
+				const medal = medals[index]
+				fieldsTop.push({
+					name: `Top ${index + 1}`,
+					value: `${medal} **${user.userName}** - Nivel \`${user.level}\`・\`${user.xp}\` xp`,
+				})
+			}
+
+			for (const [index, user] of top4to10.entries()) {
+				fieldsTop.push({
+					name: `Top ${index + 4}`,
+					value: `<:purplearrow:1129823325170958457> **${user.userName}** - Nivel \`${user.level}\`・\`${user.xp}\` xp`,
+				})
+			}
+
+			topEmbed.addFields(fieldsTop)
+
+			int.reply({ embeds: [topEmbed] })
+		},
+	},
 }
