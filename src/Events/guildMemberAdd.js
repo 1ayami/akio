4c94a3501e6f07bf5d06f3f75affedd2edbe1365
ayami <ayami101106@gmail.com>
2023-12-29 19:53:55 -0500

    correcciones

--- src/Events/guildMemberAdd.js
+++ src/Events/guildMemberAdd.js
@@ -10,6 +10,12 @@ module.exports = {
 	 * @param { GuildMember } member
 	 */
 	exe(bot, member) {
+		const general = member.guild.channels.cache.find(
+			(c) => c.name == '・💬・general'
+		)
+		const inicio = member.guild.channnels.cache.find(
+			(c) => c.name == '・🌌・inicio'
+		)
 		// Embed de bienvenida
 		const embedWLC = new EmbedBuilder()
 			.setColor(bot.config.colors.normal)
@@ -17,12 +23,12 @@ module.exports = {
 			.setAuthor({ name: 'Genesis*', iconURL: member.guild.iconURL() })
 			.setTitle(`> Un nuevo usuario ha ingresado <:ztar:902305429474934824>!`)
 			.setDescription(
-				`Bienvenid@ ${member} a **Genesis**!, si necesitas saber algo, te recomiendo revisar el canal <#1072240599081689119>. Ten la total libertad de explorar los canales y resolver cualquier duda que tengas con los moderadores o dueño del servidor!\n\nPuedes ir a saludar a <#1110740317935767612>!`
+				`Bienvenid@ ${member} a **Genesis**!, si necesitas saber algo, te recomiendo revisar el canal ${inicio}. Ten la total libertad de explorar los canales y resolver cualquier duda que tengas con los moderadores o dueño del servidor!\n\nPuedes ir a saludar a ${general}!`
 			)
 			.setImage('https://i.imgur.com/N1r89gK.jpeg')
 
 		const canal = member.guild.channels.cache.find(
-			(c) => c.name == '・💬・general'
+			(c) => c.name == '・🌌・nuevos'
 		)
 
 		canal.send({ content: `${member}`, embeds: [embedWLC] })
